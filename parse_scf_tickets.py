55f754007e5a38b2d46c2fe1c81d1b60be15d5d4
Jenny Turner-Trauring <jenineturner@gmail.com>
2018-03-27 13:43:22 -0400

    Parsing see click fix tickets from json files to look for snowy bike lanes

---
+++ parse_scf_tickets.py
@@ -0,0 +1,23 @@
+import argparse
+import json
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("jsonfile", help="json see click fix file")
+    args = parser.parse_args()
+
+    count = 0
+    with open(args.jsonfile) as data_file:
+        data = json.load(data_file)
+        for d in data:
+
+            if 'title' in d['request_type'].keys() and d[
+                    'request_type']['title'] == 'Icy or Snowy Bike Lane':
+                print d
+                print "\n"
+
+                count += 1
+    print count
+
+
